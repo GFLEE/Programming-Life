ded63c4f62aa85d0905b58f2775fad673c7e632c
LEE <leegradylll@gmail.com>
2017-09-04 18:57:27 +0800

    choose_sort( )

---
+++ choose_sort().cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int sort(int arr[],int n)
+{
+	for(int m=0;m<n-1;m++)
+	{
+		for(int j=m+1;j<n;j++)
+		{
+			if(arr[m]>arr[j])
+			{
+				int k=arr[m];
+				   arr[m]=arr[j];
+				   arr[j]=k;
+			}
+			
+		}
+	
+	}
+
+	return 0;
+}
+int main(void)
+{
+	int arr[]={9,8,7,6,5,4,3,2,1,0};
+	for(int i=0;i<10;i++)
+	cout<<arr[i]<<" ";
+	
+	sort(arr,10);
+	cout<<"\n";
+	for(int i=0;i<10;i++)
+     cout<<arr[i]<<" ";	
+	
+	
+	return 0;
+}
