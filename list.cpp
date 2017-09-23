99069f59bda8a51eeba5f4a599dbf733cd259d31
gflee <leegradylll@gmail.com>
2017-09-23 23:39:22 +0800

    CRAP

--- list.cpp
+++ list.cpp
@@ -23,47 +23,42 @@ int ListInsert(SqList &L,int i,int e)
 {
 	
 if(i<1||i>L.length+1)
-return 0;	//判断i值是否合法 
+return 0;	                   //judge whether i is legal,i值是否合法 
 	if(L.length<L.listsize)
 	{
-		int *newbase=(int *)realloc(L.elem,L.listsize+sizeof(int)*10); //realloca函数为表开辟新的空间 
-		if(!newbase)     //申请失败 
+		int *newbase=(int *)realloc(L.elem,L.listsize+sizeof(int)*10); //increase space;realloca函数为表开辟新的空间 
+		if(!newbase)          //fail 申请失败 
 		exit(-2);
-		L.elem=newbase; //将首地址重新赋给elem 
-		L.listsize+=10;   //容量增加 
+		L.elem=newbase;      //renew the address of first,将首地址重新赋给elem 
+		L.listsize+=10;       //容量增加 
 	}
-	int *p;      //数组尾指针 
-	int *q=&(L.elem[i-1]);   //待插入位置指针 
-    for(p=&(L.elem[L.length-1]);p>=q;p--)     //尾指针p前移，使前面的元素依次后移将q所指空间腾出 
+	int *p;                  //pointer of tail,数组尾指针 
+	int *q=&(L.elem[i-1]);   //point of that ready to insert,待插入位置指针 
+    for(p=&(L.elem[L.length-1]);p>=q;p--)     //tail pointer move forward,and values move backward,尾指针p前移，使前面的元素依次后移将q所指空间腾出 
     {  *(p+1)=*p;   }
-     *p=e;            //将数值插入 
-     L.length++;     //长度加 1 
+     *p=e;                  //将数值插入 
+     L.length++;          //length +1 
 
 return 1;	
 }
  
 
-int DisPlayList(SqList &L)
-{
-	int i;
-	for(i=0;i<L.length;i++)
-	printf("%d+",L.elem[i]);
-	
-	return 0;
-	
-}
+
 
 
 int main()
 {
 
-    SqList L;   
+    SqList L;                    //create list.
     InitList(L);
     
     
 	ListInsert(L,1,7);
+
 	
-	  DisPlayList(L);
+	for(int i=0;i<L.length;i++)   //show all
+	printf("%d",L.elem[i]);
+	 
 	  
 
 
