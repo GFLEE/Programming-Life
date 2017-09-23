5eec132e9098f541931afc7edd570b5133a1b646
gflee <leegradylll@gmail.com>
2017-09-23 22:53:34 +0800

    none

--- list.cpp
+++ list.cpp
@@ -22,32 +22,52 @@ int InitList (SqList &L)          //实参传递，函数对表才能进行数据操作
 int ListInsert(SqList &L,int i,int e)
 {
 	
-if(i<1&&i>L.length+1)
+if(i<1||i>L.length+1)
 return 0;	//判断i值是否合法 
 	if(L.length<L.listsize)
 	{
-		int *newbase=(int *)realloc(L.elem,L.listsize+sizeof(int)*10);
-		if(!newbase)
+		int *newbase=(int *)realloc(L.elem,L.listsize+sizeof(int)*10); //realloca函数为表开辟新的空间 
+		if(!newbase)     //申请失败 
 		exit(-2);
-		L.elem=newbase;
-		L.listsize+=10;
+		L.elem=newbase; //将首地址重新赋给elem 
+		L.listsize+=10;   //容量增加 
 	}
-	int *p;
-	int *q=&(L.elem[i-1]);
-    for(p=&(L.elem[L.length-1]);p>=q;p--)
+	int *p;      //数组尾指针 
+	int *q=&(L.elem[i-1]);   //待插入位置指针 
+    for(p=&(L.elem[L.length-1]);p>=q;p--)     //尾指针p前移，使前面的元素依次后移将q所指空间腾出 
     {  *(p+1)=*p;   }
-     *p=e;
-     L.length++;
+     *p=e;            //将数值插入 
+     L.length++;     //长度加 1 
 
 return 1;	
 }
  
 
+int DisPlayList(SqList &L)
+{
+	int i;
+	for(i=0;i<L.length;i++)
+	printf("%d+",L.elem[i]);
+	
+	return 0;
+	
+}
+
 
 int main()
 {
+
+    SqList L;   
+    InitList(L);
+    
+    
+	ListInsert(L,1,7);
 	
-	
+	  DisPlayList(L);
+	  
+
+
+	 
 	
 	return 0;
 }
