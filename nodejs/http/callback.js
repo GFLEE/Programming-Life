7c7ea6e42bad6b26e0ebfb2eb6150c4cce9983bd
gflee <leegradylll@gmail.com>
2017-11-03 22:03:26 +0800

    node

--- nodejs/http/callback.js
+++ nodejs/http/callback.js
@@ -48,22 +48,53 @@
 // }
 // pet("................................")
 
+// console.log('==================xx');
 
-console.log('==================xx');
+// function pet(words){
+//     this.words=words;
+//     this.speak=function(){
 
-function pet(words){
-    this.words=words;
-    this.speak=function(){
+//         console.log(this.words);
 
-        console.log(this.words);
+//     }
 
-    }
+// }
+// var cat=new pet('miao!');
+// cat.speak();
 
-}
-var cat=new pet('miao!');
-cat.speak();
+// var pet= {
+//   words:'pet words',
+//   speak:function(say){
+//       console.log(say+this.words)
+//   }
+// }
 
+// var dog={                 //函数调用之call（）调用，人为地把this绑定到dog，通过call（）改变执行上下文
 
+//     words:'I am a dog.'
+// }
+// pet.speak.call(dog,'dog say:')
 
+// call/apply()继承
+// function animal(words){
+//     this.words=words;
+//   this.speak=function(words){
+//         console.log(this.words)
+//     }
+// }
 
+// var dog=function(words){
+//       animal.call(this,words);
+// }
+// var DOG=new dog('dogge!');
+// DOG.speak();
+// var arr=new Array(100);
+// arr[0]=1;
+// arr[1]=1;
+// for(var i=2;i<100;i++)
+// {
+//     arr[i]=arr[i-1]+arr[i-2];
 
+// }
+// for(let i=0;i<100;i++)
+// console.log(arr[i]);
