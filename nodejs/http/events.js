95fa5c059ecfe00e56b891226027508acfb04281
gflee <leegradylll@gmail.com>
2017-11-04 23:43:01 +0800

    event

---
+++ nodejs/http/events.js
@@ -0,0 +1,21 @@
+var events = require("events"); //获取event模块
+var EventEmitter = events.EventEmitter;  //得到EVentEmitter对象
+var main = new EventEmitter();            //将该对象实例化
+main.on("trigg", function(pram) {        //绑定事件监听，回调函数，trigg事件被触发
+  console.log("hello," + pram);
+});
+main.on("trigg", function(pram) {        //绑定事件监听，回调函数，trigg事件被触发
+    console.log("hello," + pram);
+  });main.on("trigg", function(pram) {        //绑定事件监听，回调函数，trigg事件被触发
+    console.log("hello," + pram);
+  });
+  main.on("trigg", function(pram) {        //绑定事件监听，回调函数，trigg事件被触发
+    console.log("hello," + pram);
+  });
+  main.on("trigg", function(pram) {        //绑定事件监听，回调函数，trigg事件被触发
+    console.log("hello," + pram);
+  });
+  main.on("trigg", function(pram) {        //绑定事件监听，回调函数，trigg事件被触发
+    console.log("hello," + pram);
+  });
+main.emit("trigg", 'Lee');
