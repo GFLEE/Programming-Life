fe0ab93a2f426d6a8b1bcb471e9f462231e1183b
gflee <leegradylll@gmail.com>
2017-11-04 17:43:09 +0800

    success

--- nodejs/http/crawler.js
+++ nodejs/http/crawler.js
@@ -1,11 +1,54 @@
 var http = require("http");
-var cheerio = require("./node_modules/cheerio/lib/cheerio.js");
-var url = "http://course.casvot.com";
+var cheerio = require("cheerio");
+var url = "http://www.imooc.com/learn/348";
 function filter(html) {
   var $ = cheerio.load(html);
-  var topics = $(".entry-title");
-  console.log(topics);
+  var chapters=$('.chapter');
+
+
+  console.log(chapters)
+  var courseData=[]
+  chapters.each(function(item){
+
+    var chapter=$(this);
+    var chapterTitle=chapter.find('strong').text()
+    var videos=chapter.find('.video').children('li')
+
+    var chapterData={
+        chapterTitle:chapterTitle,
+        videos:[]
+    }
+ videos.each(function(){
+     var video=$(this).find(".J-media-item")
+         var videoTitle=video.text()
+         var id=video.attr('href').split('video/')[1]   
+       chapterData.videos.push({
+           title:videoTitle,
+           id:id
+
+       })
+ })
+
+ courseData.push(chapterData)
+  })
+
+return courseData;
+
 }
+;
+function printCourseData(courseData){
+courseData.forEach(function(item){
+    var chapterTitle=item.chapterTitle
+    console.log(chapterTitle+'\n')
+    item.videos.forEach(function(video)
+{
+     console.log('【'+video.id+'】'+video.title+'\n')
+
+})
+})
+}
+
+
 http
   .get(url, function(res) {
     var html = " ";
@@ -13,7 +56,11 @@ http
       html += data;
     });
     res.on("end", function() {
-      filter(html);
+       
+      var courseData=filter(html);
+
+      printCourseData(courseData);
+
     });
   })
   .on("error", function() {
