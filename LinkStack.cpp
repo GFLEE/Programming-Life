77b0a4f615d531367b17d33f6501cebfb85c8d94
Lee <leegradylll@gmail.com>
2018-05-24 22:19:34 +0800

    data structure-Stack

---
+++ LinkStack.cpp
@@ -0,0 +1,119 @@
+#include "iostream"
+using namespace std;
+#include "stdlib.h"
+#define null 0
+#define Null 0
+
+typedef struct LinkStack{
+	
+	int data;
+    struct LinkStack *next;
+
+}Stack;
+
+//=================create a linkstack=======================
+
+void createStack(Stack *&s)
+{
+	s=(Stack *)malloc(sizeof(Stack)); //the first node
+    s->next=null;	
+}
+
+//================push a element to the stack=============
+    int Push(Stack *&s,int val)
+    {
+    	Stack *new_node;
+    	
+    	new_node=(Stack *)malloc(sizeof(Stack));
+    	
+    	new_node->data=val;
+    	new_node->next=s->next;        //create a new node
+    	
+    	s->next=new_node;
+//    	s=new_node;  we did not do this ,always make 's' ponit to the head node.....
+//because first in last out....always insert new node after head node
+	}
+	//notice: in LinkList,insert after head node is similar with this!!!!!!!!
+//=============Pop a val========================
+  int  Pop(Stack *&s)
+  {
+  	 Stack *p;
+  	  int temp;
+  	  if(s->next==null)
+  	  {
+  	  	cout<<"Empty Stack!"<<endl;
+		}
+		else
+		{
+	  temp=s->next->data;
+  	  cout<<"Pop: "<<temp<<endl;
+  	  s->next=s->next->next;       //head node point to the third node 
+  	  return temp;
+		}
+	  }	
+//=====================display all============================
+
+int displayStack(Stack *&s)
+{
+	Stack *p=s->next;     //use 'p' to Traversal the Stack
+	int len=0;
+	
+	if(s->next==null)
+	{
+		cout<<"Empty stack!"<<endl;
+		return 0;
+	}
+	else{
+		
+		while(p!=null)
+		{
+			int temp=p->data;
+			 cout<<temp<<" ";
+			 p=p->next;
+			 len++;    // when a node traversaled finished, len+1
+		}
+		
+	  cout<<endl<<"Stack Length is: "<<len<<endl;
+	  return len;
+		
+	}
+}
+
+
+//===========================destroy Stack============================
+
+	void destroyStack(Stack *s)
+	{
+		
+		Stack *p=s->next;
+		while(p!=null)
+		{
+		   free(s);
+		   s=p;
+		   p=p->next;	
+		}
+		
+		free(s);
+		
+		cout<<"Stack Destroy finished."<<endl;
+	}
+
+
+//============================
+int main()
+{
+    Stack *s;     //this pointer , we use it as the top pointer	
+	createStack(s);
+	
+	for(int i=0;i<20;i++)
+	{
+		Push(s,i);
+	}
+	
+	displayStack(s);
+//	destroyStack(s);
+//    displayStack(s);
+
+	return 0;
+}
+
---
+++ Seqstack.cpp
@@ -0,0 +1,161 @@
+#include "iostream"
+using namespace std;
+#include "stdlib.h"
+
+#define maxsize 10
+	typedef struct stack{
+		
+		int data[maxsize];
+		int top;
+		
+	}Stack;
+//===============init a stack==========
+void initStack(Stack *&s)
+{
+	s=(Stack *)malloc(sizeof(Stack));
+	s->top=-1;
+}
+
+//==============push()===============
+
+int push(Stack *&s,int val)
+{
+	if(s->top==maxsize-1)
+	{
+		cout<<"The stack is up overflow! Can't write more!"<<endl;
+		return 0;
+	}
+	else{
+		s->top++;
+//		cout<<s->top<<' ';      test 'top'
+		s->data[s->top]=val;
+		return 1;
+	}
+	
+}
+
+//====================================
+//==========pop operation=============
+int pop(Stack *&s)     //
+{
+	if(s->top==-1)
+	{
+		cout<<"Down overflow occured!"<<endl;
+		return 0;
+		
+	}
+	else{
+		
+		int temp;
+		temp=s->data[s->top];
+		s->top--;
+	    cout<<temp<<endl;
+	 	return temp;	
+	}
+	
+}
+//========================destory a array stack============
+   void destroyStack(Stack *&s)
+   {
+   	free(s);
+   }
+   
+//========================dispay a stack============
+
+	 void 	displayStack(Stack *&s)
+	 {
+	 	
+	 	int pointer=s->top;
+	 	while(pointer!=-1)
+	 	{
+	 		
+	 		cout<<s->data[pointer]<<" ";
+	 		pointer--;
+		 }
+		 cout<<endl<<endl;
+	 }
+
+//===================return lenth of the stack===========
+  int getLength(Stack *s)
+  {
+  	
+  	  int len;
+  	  len=(s->top)+1;
+
+  	  cout<<"Stack length is: "<<len<<endl;
+  	  return len;
+  }
+
+//===========is empty===========================
+  void isEmpty(Stack *&s)
+  {
+  	if(s->top==-1)
+  	{
+  		cout<<"Empty stack!"<<endl;
+	  }
+	  else
+	  {
+	  	cout<<"Stack is Not Empty."<<endl<<endl;
+	  }
+  }
+//==============================================
+
+//====================Algortham================
+
+  // write a function, test the string wheather a symmetrical string , try to use Stack to solve it.
+  
+  //because the data is 'int',so I define a number array to replace it;
+   void symmetry(int *str)
+  {
+  	   Stack *p;
+  	   initStack(p);
+  	   int flag;
+  	   
+  	   for(int i=0;i<7;i++)
+  	   {
+  	   	push(p,str[i]);
+		 }
+		 displayStack(p);
+		 int m=0;
+	 
+	   	
+	   	while(p->top!=-1)
+	   	{
+	   		
+	   		int temp=pop(p);
+	   	   if(temp==str[m])
+			  {
+			  	flag=1;
+				  }	
+				  else{
+				  flag=0;
+				  }
+				m++;
+		   
+	   }
+	   flag?cout<<"It is a symmetrical string."<<endl:cout<<"Not a symmetrical string."<<endl;
+  	
+  }
+  
+
+
+//============================================
+int main(){
+	
+	Stack *s;
+	initStack(s);
+	for(int i=0;i<12;i++)
+	{
+		push(s,i);
+	}
+	displayStack(s);
+    isEmpty(s);
+	
+	int strings[]={1,3,3,4,4,2,1};
+	symmetry(strings);
+	
+	
+	
+	return 0;      //2018.5.24
+	
+}
