efde184ee59f5440e26c5968b5c4c493fb513685
GFLEE <leegradylll@gmail.com>
2018-06-07 22:46:47 +0800

    ht

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -21,19 +21,11 @@ void createHT(HTNode ht[],int n)
 	{
 		ht[i].parent=ht[i].LNode=ht[i].RNode=-1;
 	}
-//=====================================
-	int weight_init=1;            //create some leaf nodes,put leaf nodes range '0' to 'n-1',
-                         	//and normal nodes range 'n' to '2n-2', obviously,ht[2n-1] is the rootNode.
-	for(int i=0;i<5;i++)
-	{
-		ht[i].weight=weight_init;
-		weight_init+=2;
-	 }
 //======================================
 
 //create Huffman tree
 for(int m=n;m<2*n-1;m++)    // load data to nomal node ('n' to last node )   
-{
+	{
 
 	int min1,min2;     //the minimum weight of two of all the leaf nodes.
 	    min1=min2=weight_MAX;	
@@ -41,55 +33,62 @@ for(int m=n;m<2*n-1;m++)    // load data to nomal node ('n' to last node )
 	int leftNode,rightNode;   //confirm  the final left or right node  location
 		leftNode=rightNode=-1;
 	
-	for(int i=0;i<m;i++)     //select minimum weight of two from leaf nodes.
+	for(int i=0;i<m;i++)     //select minimum weight of two from leaf nodes and new node has no parent.
 	{
-		if(ht[i].parent=-1)
+		if(ht[i].parent==-1)    //choose the left/right child node from  node which has no parent
 		{
-			if(ht[i].weight<min1)
+			if(ht[i].weight<min1)    //send the current_data to right part,then recieve the less weight node
 			{
+				min2=min1;
+				min1=ht[i].weight;
+				rightNode=leftNode;   //jesus
+				leftNode=i;
 				
 			}
 			else 
-			if(ht[i].weight<min2)
+			if(ht[i].weight<min2)     //min1 doesnt need the node that weight greater than itself. 
+		//but the weight less than min2,so min2 will recieve this node_weight
 			{
-				
-				
+				min2=ht[i].weight;
+				rightNode=i;
 			}
+			//if the weight greater than min1&min2,still choose two minimum weight,because the min1&min2 have return to 32767 
 			
-		}
-		
+		}	
 	}
+	//========build a relationship between and childs============
+	ht[m].weight=ht[leftNode].weight+ht[rightNode].weight;
+	ht[m].LNode=leftNode;
+	ht[m].RNode=rightNode;
 	
-	
-	
-	
-	
-	
-	
+	ht[leftNode].parent=m;
+	ht[rightNode].parent=m;
+
+
+	}	
 }
 
 
-
-
-	 
-
-	
-	
-	
-	
-}
 int main()
 {
 	HTNode ht[MAX];
 	
+     
+	ht[0].weight=1;
+	ht[1].weight=4;
+	ht[2].weight=6;
+	ht[3].weight=7;
+    ht[4].weight=9;
 
-	
 	 
 	createHT(ht,5);
 	
 	
 
-	  
+	  for(int k=0;k<9;k++)
+	  {
+	  	cout<<ht[k].weight<<',';
+	  }
 	  
 	  
 	  
