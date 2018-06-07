c5288d1b6ac2edcfc251beef1550d7717ee7699f
GFLEE <leegradylll@gmail.com>
2018-06-07 17:05:50 +0800

    ht

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -1,19 +1,62 @@
 #include "iostream"
 using namespace std;
-typedef struct 
+#include <stdio.h>
+#define MAX 100
+
+typedef struct    //tree  Node stucture
 {
 	
 	int data;
-	double weight;
+	int weight;
+	int parent;
 	int LNode;
 	int RNode;
-0	
+
+}HTNode;
+
+void createHT(HTNode ht[],int n)
+{
+		for(int i=0;i<2*n-1;i++)     //all the tree nodes initiate
+	{
+		ht[i].parent=ht[i].LNode=ht[i].RNode=-1;
+	}
+//=====================================
+	int weight_init=1;            //create some leaf nodes
+	for(int i=0;i<5;i++)
+	{
+		ht[i].weight=weight_init;
+		weight_init+=2;
+	 }
+//======================================
+
+
+
+
+
+
+	 
+
+	
+	
+	
+	
 }
 int main()
 {
+	HTNode ht[MAX];
+	
+
+	
+	 
+	createHT(ht,5);
 	
 	
-	
-	
+
+	  
+	  
+	  
+	  
+	  
+	  
 	return  0;
  } 
